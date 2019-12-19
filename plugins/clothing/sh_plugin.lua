6240655eb0faa355150413d31e3ef873b9536053
DESKTOP-F3EK6GQ\calho <calhoun.carl@gmail.com>
2019-12-19 00:23:44 -0500

    Correcting clothing plugin version.

--- plugins/clothing/sh_plugin.lua
+++ plugins/clothing/sh_plugin.lua
@@ -1,15 +1,18 @@
 local PLUGIN = PLUGIN
-PLUGIN.name = "Advanced Clothing System"
+PLUGIN.name = "Clothing System"
 PLUGIN.author = "Shavargo"
 PLUGIN.desc = "Clothing system utilizing bonemerging."
 
 
-function PLUGIN:PlayerPostThink(client)
-	local torso = ents.Create("prop_physics")
-		torso:SetModel("models/projectpt/suits/male_anorak.mdl")
-		torso:SetParent(client, 1)
-		torso:SetPos(Vector(0, 0, 0))
-		torso:SetAngles(Angle(0,0,0))
-
-		torso:AddEffects(EF_BONEMERGE)
+function PLUGIN:PlayerLoadedCharacter(client, curChar, prevChar)
+	if SERVER then
+		if client:GetCharacter() then
+			local torso = ents.Create("prop_physics")
+				torso:SetModel("models/projectpt/suits/male_anorak.mdl")
+				torso:SetParent(client, 1)
+				torso:SetPos(Vector(0,0,0))
+				torso:SetAngles(Angle(0,0,0))
+				torso:AddEffects(EF_BONEMERGE)
+		end
+	end
 end

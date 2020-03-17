e435d7911dc5575e04c8e45577ae4fdf49b0f9a2
Shavargo <calhoun.carl@gmail.com>
2020-03-17 16:12:26 -0400

    Crafting attributes.

---
+++ schema/attributes/sh_gunsmith.lua
@@ -0,0 +1,6 @@
+
+-- You can define attributes in the attributes/ folder. The unique ID of the attribute will be the same as the name of the file.
+
+ATTRIBUTE.name = "Gunsmith Knowledge"
+ATTRIBUTE.description = "Your knowledge of guns."
+ATTRIBUTE.noStartBonus = true -- You cannot spend points to upgrade this attribute when creating the character

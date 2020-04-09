568b44ca4c1aa08a90bceda61a795fc811da07dc
Shavargo <calhoun.carl@gmail.com>
2020-04-09 15:22:06 -0400

    Change default models for factions.

---
+++ plugins/stalkerclothing/sh_plugin.lua
@@ -0,0 +1,24 @@
+PLUGIN.name = "Stalker Clothing"
+PLUGIN.author = "Shavargo"
+PLUGIN.desc = "Adds clothing items"
+
+CITIZEN_MODELS = {
+	"models/tnb/stalker/female_01_anorak.mdl",
+	"models/tnb/stalker/female_02_anorak.mdl",
+	"models/tnb/stalker/female_03_anorak.mdl",
+	"models/tnb/stalker/female_04_anorak.mdl",
+	"models/tnb/stalker/female_05_anorak.mdl",
+	"models/tnb/stalker/female_06_anorak.mdl",
+	"models/tnb/stalker/female_07_anorak.mdl",
+	"models/tnb/stalker/female_08_anorak.mdl",
+	"models/tnb/stalker/male_01_anorak.mdl",
+	"models/tnb/stalker/male_02_anorak.mdl",
+	"models/tnb/stalker/male_03_anorak.mdl",
+	"models/tnb/stalker/male_04_anorak.mdl",
+	"models/tnb/stalker/male_05_anorak.mdl",
+	"models/tnb/stalker/male_06_anorak.mdl",
+	"models/tnb/stalker/male_07_anorak.mdl",
+	"models/tnb/stalker/male_08_anorak.mdl",
+	"models/tnb/stalker/male_09_anorak.mdl",
+	"models/tnb/stalker/male_10_anorak.mdl"
+}
